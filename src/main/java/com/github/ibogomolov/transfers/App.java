c880d5c916423ffe23ac8b7430b02cfa61b02acd
Igor Bogomolov <igor.bogomolov@gmail.com>
2019-11-04 03:41:45 +0100

    implement app's main method

--- src/main/java/com/github/ibogomolov/transfers/App.java
+++ src/main/java/com/github/ibogomolov/transfers/App.java
@@ -3,12 +3,30 @@
  */
 package com.github.ibogomolov.transfers;
 
+import com.github.ibogomolov.transfers.datamodel.Account;
+import com.github.ibogomolov.transfers.datastore.AppDatastore;
+import com.github.ibogomolov.transfers.handler.CreateMoneyTransferHandler;
+import com.github.ibogomolov.transfers.handler.GetAccountHandler;
+import com.github.ibogomolov.transfers.handler.GetMoneyTransferHandler;
+import lombok.extern.slf4j.Slf4j;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+
+@Slf4j
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
-    }
+
+    private static final AppDatastore datastore = new AppDatastore();
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        datastore.accounts.insert(new Account("Isaac", "Newton", 1000.0));
+        datastore.accounts.insert(new Account("Charles", "Darwin", 1000.0));
+        datastore.accounts.insert(new Account("Harry", "Potter", 1000.0));
+
+        post("/transfer", "application/json", new CreateMoneyTransferHandler(datastore));
+
+        get("/account/:id", new GetAccountHandler(datastore));
+
+        get("/transfer/:id", new GetMoneyTransferHandler(datastore));
     }
 }
